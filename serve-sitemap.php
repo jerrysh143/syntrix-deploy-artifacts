4c58fc8a1cd4017633cd009b05070dbb73cc20a5
jay soni <29036751+jerrysh143@users.noreply.github.com>
2026-09-23 19:55:33 +0000

    Rewrite sitemap.xml to serve-sitemap.php; robots same-origin only
    Aim to avoid LiteSpeed/hCDN .xml 500 before Passenger; keep PHP as body source.

--- serve-sitemap.php
+++ serve-sitemap.php
@@ -1,10 +1,12 @@
 <?php
 header('Content-Type: application/xml; charset=utf-8');
+header('X-Content-Type-Options: nosniff');
+header('Cache-Control: public, max-age=300');
+header('X-Robots-Tag: noindex');
 $path = __DIR__ . '/sitemap.txt';
 if (!is_readable($path)) {
   http_response_code(404);
-  header('Content-Type: application/xml; charset=utf-8');
-  echo '<?xml version="1.0" encoding="UTF-8"?><error>missing sitemap.txt</error>';
+  echo '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></urlset>';
   exit;
 }
 readfile($path);

c1ee1c8c7db5303add3e3b2d219760a3111c1dea
jay soni <29036751+jerrysh143@users.noreply.github.com>
2026-09-23 19:35:26 +0000

    sitemap: no physical .xml; panel redirect to serve-sitemap.php

--- serve-sitemap.php
+++ serve-sitemap.php
@@ -1,9 +1,9 @@
 <?php
 header('Content-Type: application/xml; charset=utf-8');
-header('X-Robots-Tag: noindex');
 $path = __DIR__ . '/sitemap.txt';
 if (!is_readable($path)) {
   http_response_code(404);
+  header('Content-Type: application/xml; charset=utf-8');
   echo '<?xml version="1.0" encoding="UTF-8"?><error>missing sitemap.txt</error>';
   exit;
 }
